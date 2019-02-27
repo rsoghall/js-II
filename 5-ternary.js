fbcaaf30502dc2001af28c94e65dda2d735db331
Ryan <ryan@dksit.com>
2019-02-27 12:43:10 -0700

    added ternary exmaples

--- 5-ternary.js
+++ 5-ternary.js
@@ -1,11 +1,24 @@
 // *** TERNARY *** //
 
 // Let's review if/else statements
+let name = 'Ryan';
+if(name !='Ryan'){
+    name ='Ryan'
+} else {
+    name = 'John'
+}
+console.log(name)
+
 
 // What if we could write the same thing, but with less typing?
+let x = name === 'John' ? true : false
 
+console.log(x)
 // How do we use an if/else statement to assign a variable?
 
 // How do we do it with a ternary?
 
 // Can we nest ternaries?
+y = -1
+let thing = y > 0 ? 'its positive' : y === 0 ? 'its zero' : 'its negative'
+console.log(thing)
